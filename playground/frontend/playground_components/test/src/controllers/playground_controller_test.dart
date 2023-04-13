544b1e042946f342d479a1cb3660dc89fc77f561
darkhan.nausharipov <darkhan.nausharipov@kzn.akvelon.com>
2023-04-13 16:06:58 +0600

    FeedbackController in PlaygroundComponents

--- playground/frontend/playground_components/test/src/controllers/playground_controller_test.dart
+++ playground/frontend/playground_components/test/src/controllers/playground_controller_test.dart
@@ -17,7 +17,6 @@
  */
 
 import 'package:flutter_test/flutter_test.dart';
-import 'package:get_it/get_it.dart';
 import 'package:mockito/annotations.dart';
 import 'package:mockito/mockito.dart';
 import 'package:playground_components/playground_components.dart';
@@ -30,7 +29,6 @@ import 'playground_controller_test.mocks.dart';
 Future<void> main() async {
   TestWidgetsFlutterBinding.ensureInitialized();
   await PlaygroundComponents.ensureInitialized();
-  GetIt.instance.registerSingleton(FeedbackController());
 
   late PlaygroundController controller;
   final mockExamplesLoader = MockExamplesLoader();
